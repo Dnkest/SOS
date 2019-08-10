da3555f4b42f7a58232ed798993705954c828de2
rootuserhubery <z5092145@ad.unsw.edu.au>
2019-08-10 15:16:52 +1000

    test app

---
+++ projects/aos/apps/muo_gui_ce_shi/src/muo_gui_ce_shi.c
@@ -0,0 +1,32 @@
+
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/time.h>
+#include <utils/time.h>
+#include <utils/page.h>
+#include <syscalls.h>
+
+#include <sos.h>
+
+size_t sos_write(void *vData, size_t count)
+{
+    //implement this to use your syscall
+    return sos_sys_write(3, vData, count);
+}
+
+int main(void)
+{
+    sosapi_init_syscall_table();
+
+    int in = open("console", 1);
+    assert(in >= 0);
+
+    printf("task:\tHello world, I'm muo gui!\n");
+    return 0;
+}
